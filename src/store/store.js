5114428ca9a69209087d5e7df6d6ccc708c84122
Abdulkhafiz Yoqubjonov <psfer10green10@gmail.com>
2025-04-15 20:52:54 +0500

    feat: add computers reducer to Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import headphonesReducer from './headphonesSlice';
 import cartReducer from "./cartSlice"
+import computersReducer from './computersSlice';
+
 const store = configureStore({
   reducer: {
     headphones: headphonesReducer,
     cart: cartReducer,
+    computers: computersReducer,
   },
 });
 
